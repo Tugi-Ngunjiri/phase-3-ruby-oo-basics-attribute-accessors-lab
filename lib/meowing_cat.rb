5e179c72679ba347b5c73df90e23299783ddd4ca
tugi <blueqwinn@gmail.com>
2023-02-09 13:22:04 +0300

    first commit

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,14 @@
 ## code your solution here. 
+class Cat
+attr_accessor :name
+Cat = Cat.new
+cat.name = "Cat "
+cat.name
+#=> "Cat "
+ end
+end
+
+Class Meow 
+attr_accessor: meow
+puts "meow!"
+end
